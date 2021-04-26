0d881637210c8c584cfd3af46ec7980b96f0fbd0
hwee1115 <hwee1115@naver.com>
2021-04-26 17:47:06 +0900

    명휘커밋

--- appRoute.js
+++ appRoute.js
@@ -3,10 +3,11 @@ angular.module("app")
      console.log("app-config callback(ROUTE)");
      
  $routeProvider
+ .when("/", {templateUrl: "views/main/home.html",controller:"MainController"})
 .when("/http_order", {templateUrl: "views/http_order/index.html",controller:"OrderController"})
 .when("/http_reviews", {templateUrl: "views/http_reviews/index.html", controller:"ReviewController"})
 .when("/http_user", {templateUrl: "views/http_user/index.html", controller: "UserController"})
 .when("/http_qna",{templateUrl: "views/http_qna/index.html", controller: "QnaController"})
 .when("/product", {templateUrl:"views/Product/index.html", controller:"ProductController"})
-.otherwise({redirectTo: "/http_user"}) 
+.otherwise({redirectTo: "/"}) 
 });
