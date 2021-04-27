2b9a52d05d59a8e7c5c69a8e08a95732fbae4638
Bongsanggeun <bbsj1058@naver.com>
2021-04-27 14:43:34 +0900

    sanggeun commit

--- services/UsersService.js
+++ services/UsersService.js
@@ -8,24 +8,21 @@ angular.module("app")
         const promise = $http.post(BASE_URL + "/login", user) // auth컨트롤러에서 postmapping 방식이였으므로
         return promise;
       },
-      list: function(pageNo=1) {
-        const promise = $http.get(BASE_URL + "/", {params:{pageNo}}); // 비동기처리해야하므로 promise를 리턴해야
+      list: function(pageNo=1,keyword) { 
+        console.log("serpageno : ",pageNo)
+        console.log("serkeyword", keyword)       
+        const promise = $http.get(BASE_URL + "/keywordlist", {params:{pageNo,keyword}}); // 비동기처리해야하므로 promise를 리턴해야
         return promise;
       },
       read: function(user_id) {
         const promise = $http.get(BASE_URL + "/" + user_id); //** 
         return promise;
-      },
+      },      
+
 
       update: function(user) {
         const promise = $http.put(BASE_URL, user); 
         return promise;
-      },
-
-      delete: function(user_id) {
-        const promise = $http.delete(BASE_URL + "/" + user_id);
-        return promise;
       }
-
     }
   });
